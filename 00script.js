ee36fc746c1dd1a99bbe6f43e95eec494ee282c2
Rob Pear <80742973+tuanst130222@users.noreply.github.com>
2024-02-01 04:22:24 +0700

    Update 00script.js

--- 00script.js
+++ 00script.js
@@ -56,7 +56,7 @@ function countSequentialMatches(itemWords, words) {
         }
     }
 
-    // Sử dụng một hàm để tính điểm dựa trên count và totalDistance
+    // tính điểm dựa trên count và totalDistance
     return calculateScore(count, totalDistance);
 }
 
@@ -97,7 +97,7 @@ function displayResults(results) {
         
         const speakerIcon = document.createElement('span');
         speakerIcon.className = 'speaker-icon';
-        speakerIcon.innerHTML = '&#128266;'; // Unicode cho biểu tượng loa
+        speakerIcon.innerHTML = '&#128266;'; // Unicode biểu tượng loa
         speakerIcon.onclick = () => speakText(item.en);
         enP.appendChild(speakerIcon);
 
@@ -117,16 +117,13 @@ function displayResults(results) {
 let textSpeeds = {};
 
 function speakText(text) {
-    // Hủy bỏ bất kỳ âm thanh nào đang được phát
+    // Hủy bỏ bất kỳ âm thanh đang được phát
     speechSynthesis.cancel();
 
     const utterance = new SpeechSynthesisUtterance(text);
     let voices = speechSynthesis.getVoices();
-
-    // Chọn giọng đọc tiếng Anh Mỹ
     let selectedVoice = voices.find(voice => voice.lang === 'en-US');
 
-    // Điều chỉnh giọng đọc và độ cao
     if (selectedVoice) {
         utterance.voice = selectedVoice;
         utterance.pitch = 2;
@@ -159,113 +156,3 @@ document.addEventListener('keypress', function(event) {
         document.getElementById('searchInput').focus();
     }
 });
-
-
-// results.sort((b, a) => {
-//     const aScore = calculateScore(a, searchWords);
-//     const bScore = calculateScore(b, searchWords);
-
-//     return bScore - aScore; // Sắp xếp giảm dần theo điểm số
-// });
-// function calculateScore(item, words) {
-//     const matchCount = countMatches(item, words);
-//     const textLength = (item.en + " " + item.vi).length;
-//     return matchCount * 10 + textLength; // Điểm số dựa trên số từ khớp và độ dài chuỗi
-// }
-
-//****************************************** */
-// function displayResults(results) {
-//     const resultsDiv = document.getElementById('results');
-//     resultsDiv.innerHTML = '';
-
-//     if (results.length === 0) {
-//         resultsDiv.innerHTML = '<p>No results found</p>';
-//         return;
-//     }
-
-//     const maxResults = 500;
-//     const displayedResults = results.slice(0, maxResults);
-//     const searchWords = document.getElementById('searchInput').value.match(/\S+/gi) || [];
-
-//     displayedResults.forEach((item) => {
-//         const enP = document.createElement('p');
-//         enP.innerHTML = `En: ${highlightSearchTerms(item.en, searchWords)}`;
-//         resultsDiv.appendChild(enP);
-
-//         const viP = document.createElement('p');
-//         viP.innerHTML = `Vi: ${highlightSearchTerms(item.vi, searchWords)}`;
-//         resultsDiv.appendChild(viP);
-
-//         const separator = document.createElement('hr');
-//         resultsDiv.appendChild(separator);
-//     });
-// }// function speakText(text) {
-//     const utterance = new SpeechSynthesisUtterance(text);
-//     speechSynthesis.speak(utterance);
-// }
-// function speakText(text) {
-//     const utterance = new SpeechSynthesisUtterance(text);
-//     let voices = speechSynthesis.getVoices();
-
-//     // Chọn giọng đọc. Ví dụ: chọn giọng đọc tiếng Anh Mỹ đầu tiên
-//     let selectedVoice = voices.find(voice => voice.lang === 'en-US');
-
-//     // Nếu tìm thấy giọng đọc, sử dụng giọng đó
-//     if (selectedVoice) {
-//         utterance.voice = selectedVoice;
-//     }
-
-//     speechSynthesis.speak(utterance);
-// }
-
-// function speakText(text) {
-//     // Hủy bỏ bất kỳ âm thanh nào đang được phát
-//     speechSynthesis.cancel();
-
-//     const utterance = new SpeechSynthesisUtterance(text);
-//     let voices = speechSynthesis.getVoices();
-
-//     // Chọn giọng đọc tiếng Anh Mỹ
-//     let selectedVoice = voices.find(voice => voice.lang === 'en-US');
-
-//     // Kiểm tra xem đoạn văn bản đã được nhấp trước đó chưa
-//     if (clickedTexts[text]) {
-//         // Nếu đã nhấp, đọc chậm hơn
-//         utterance.rate = 0.01;
-//     } else {
-//         // Nếu chưa nhấp, đọc với tốc độ bình thường
-//         utterance.rate = 0.9;
-//     }
-
-//     // Điều chỉnh giọng đọc và độ cao
-//     if (selectedVoice) {
-//         utterance.voice = selectedVoice;
-//         utterance.pitch = 1.2;
-//     }
-
-//     // Đánh dấu văn bản này là đã được nhấp
-//     clickedTexts[text] = true;
-
-//     speechSynthesis.speak(utterance);
-// }
-
-// function speakText(text) {
-//     // Hủy bỏ bất kỳ âm thanh nào đang được phát
-//     speechSynthesis.cancel();
-
-//     const utterance = new SpeechSynthesisUtterance(text);
-//     let voices = speechSynthesis.getVoices();
-
-//     // Chọn giọng đọc tiếng Anh Mỹ
-//     let selectedVoice = voices.find(voice => voice.lang === 'en-US');
-
-//     // Điều chỉnh giọng đọc
-//     if (selectedVoice) {
-//         utterance.voice = selectedVoice;
-//         utterance.rate = 0.9; // Tốc độ đọc chậm hơn một chút
-//         utterance.pitch = 1.2; // Độ cao hơi cao
-//     }
-
-//     speechSynthesis.speak(utterance);
-// }
-
